cc64c9b5acb535aecd84718005a8e7a8505d8a8c
Rody <rody.gosset@gmail.com>
2022-12-28 17:21:17 +0100

    Started setting everything up, including the 7-1 SCSS architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+	return (
+		<>
+			<Head>
+				{/* set up favicon */}
+				<link rel="shortcut icon" href="favicon/favicon.svg" type="img/svg" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          		<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          		<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+import styles from "@styles/pages/Login.module.scss"
+import { NextPage } from "next"
+
+const Login: NextPage = () => {
+
+
+
+    return (
+        <div className={styles.container}>
+            Hello, World!
+        </div>
+    )
+}
+
+export default Login
