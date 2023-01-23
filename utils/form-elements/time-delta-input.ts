d728871e35c221f7b2f36a6c24bed4f6660d4f41
Rody <rody.gosset@gmail.com>
2023-01-23 15:57:25 +0100

    Fixed unit choosing bug

--- utils/form-elements/time-delta-input.ts
+++ utils/form-elements/time-delta-input.ts
@@ -2,7 +2,7 @@
 // utility functions for time delta inputs
 
 import { defaultTimeDeltaUnit, TimeDeltaUnit } from "@utils/types"
-import { secondsInMonth, secondsInWeek } from "date-fns"
+import { secondsInDay, secondsInMonth, secondsInWeek } from "date-fns"
 
 
 // get appropriate TimeDeltaUnit for given value (in seconds)
@@ -11,7 +11,13 @@ import { secondsInMonth, secondsInWeek } from "date-fns"
 export const getDefaultTimeDeltaUnit = (timeDelta?: number): TimeDeltaUnit => {
     // account for undefined
     if(!timeDelta) return defaultTimeDeltaUnit
-    if(timeDelta / secondsInMonth >= 1) return "months"
+    // firstly, try with exact values
+    if(timeDelta % secondsInMonth == 0) return "months"
+    else if(timeDelta % secondsInWeek == 0) return "weeks"
+    else if(timeDelta % secondsInDay == 0) return "days"
+    // if the time delta doesn't exactly match any unit, 
+    // try to find the most appropriate one
+    else if(timeDelta / secondsInMonth >= 1) return "months"
     else if(timeDelta / secondsInWeek >= 1) return "weeks"
     else return "days"
 }
