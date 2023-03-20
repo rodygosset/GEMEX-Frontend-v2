221bd654882d0a9ae1e38897573a19513dea2631
Rody <rody.gosset@gmail.com>
2023-03-20 15:52:44 +0100

    Set prod env variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,14 +14,6 @@ export const authOptions: AuthOptions = {
         CredentialsProvider({
         // The name to display on the sign in form (e.g. 'Sign in with...')
         name: 'Credentials',
-        // The credentials is used to generate a suitable form on the sign in page.
-        // You can specify whatever fields you are expecting to be submitted.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
-        credentials: {
-            username: { label: "Nom d'utilisateur", type: "text", placeholder: "Entrez votre nom d'utilisateur" },
-            password: {  label: "Matricule", type: "password" }
-        },
         // @ts-ignore
         async authorize(credentials: any) { 
 
