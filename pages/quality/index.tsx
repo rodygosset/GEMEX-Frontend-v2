f9608cf4edb8a6aab077d3bd567299c565cccad7
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-08-24 15:47:21 +0200

    Got started on the Quality Module Homepage

--- pages/quality/index.tsx
+++ pages/quality/index.tsx
@@ -1,12 +1,31 @@
+import Button from "@components/button";
+import { faArrowRight, faChartPie } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NextPage } from "next";
+import Link from "next/link";
 
 
 const Home: NextPage = () => {
 
-
     return (
-        <main className="h-full flex-1">
-            <h1>TEST</h1>
+        <main className="flex flex-col px-[7%]">
+            <div className="flex flex-row items-center gap-8">
+                <FontAwesomeIcon icon={faChartPie} className="text-5xl text-primary" />
+                <div className="flex flex-col w-full">
+                    <h1 className="text-2xl text-primary font-semibold h-fit">Taux Qualité</h1>
+                    <p className="text-md text-primary text-opacity-40 tracking-[.32rem]">TABLEAU DE BORD</p>
+                </div>
+                <Link 
+                    className="flex flex-row items-center gap-4 w-fit bg-primary bg-opacity-10 rounded-md px-[20px] py-[10px]
+                        hover:bg-opacity-20 transition duration-300 ease-in-out cursor-pointer"
+                    href="/quality/evaluations/search">
+                    <FontAwesomeIcon 
+                        icon={faArrowRight} 
+                        className="text-sm text-primary"
+                    />
+                    <span className="text-md font-normal text-primary whitespace-nowrap">Historique des évaluations</span>
+                </Link>
+            </div>
         </main>
     )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'primary': '#2962FF',
+      'secondary': '#AA00FA',
+      'success': '#16B364',
+      'warning': '#FFA200',
+      'error': '#D92D20'
+    },
+    fontFamily: {
+      'sans': ['Josefin Sans', 'sans-serif'],
+    },
+    fontSize: {
+      xs: '1.2rem',
+      sm: '1.4rem',
+      base: '1.6rem',
+      xl: '2rem',
+      '2xl': '2.4rem',
+      '3xl': '3.4rem',
+      '4xl': '4.8rem',
+      '5xl': '6rem',
+    },
     extend: {},
   },
   plugins: [],
